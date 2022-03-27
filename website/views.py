f8daa91caea19b7d6c61ff8615d012afbf248d03
Michał Witko <48209758+Majkelll@users.noreply.github.com>
2022-03-27 20:02:06 +0200

    Init signup page

--- website/views.py
+++ website/views.py
@@ -6,3 +6,12 @@ views = Blueprint("views", __name__)
 @views.route('/')
 def home():
     return render_template('./home/home.html')
+
+@views.route('/signup')
+def signup():
+    return render_template('./signup/signup.html')
+
+@views.route('/login')
+def login():
+    return render_template('./login/login.html')
+
