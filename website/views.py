0850f6e2c4cf4afda61512fd83dca28102907da3
Michał Witko <48209758+Majkelll@users.noreply.github.com>
2022-03-27 21:10:54 +0200

    login and signup page and functionality

--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import User
+from . import db
+from flask_login import login_user
 
 views = Blueprint("views", __name__)
 
@@ -15,9 +18,22 @@ def sign_up():
         firstName = request.form.get('firstName')
         passwords = [request.form.get(
             'password1'), request.form.get('password1')]
+        new_user = User(email=email, username=firstName, password=passwords[0])
+        db.session.add(new_user)
+        db.session.commit()
     return render_template('./signup/signup.html')
 
 
 @views.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+
     return render_template('./login/login.html')
